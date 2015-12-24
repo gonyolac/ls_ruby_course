ae21f3026172767aad2772867984aaba0ab6cb2e
Carlo Gonzales <gonzales.caloy@gmail.com>
2015-12-24 17:05:46 -0500

    updated blackjack code

--- lesson_4/blackjack.rb
+++ lesson_4/blackjack.rb
@@ -1,30 +1,30 @@
-require 'pry' 
+require 'pry'
 
 SUITS = ['Hearts', 'Diamonds', 'Spades', 'Clubs']
-CARD_VALUES = ['A', 2, 3, 4 ,5 ,6, 7, 8, 9, 10, 'J', 'Q', 'K']
+CARD_VALUES = ['A', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K']
+MAGIC_NUMBER = 21
 
 def prompt(msg)
   p "==> #{msg}"
 end
 
-def deal_card(plyr)   # card deal method 
-  new_deal = CARD_VALUES.sample.to_s + " of " + SUITS.sample 
-  while @player_hand.index(new_deal) || @dealer_hand.index(new_deal) do
-    new_deal = CARD_VALUES.sample.to_s + " of " + SUITS.sample  
+def deal_card(plyr) # card deal method
+  new_deal = CARD_VALUES.sample.to_s + " of " + SUITS.sample
+  while @player_hand.index(new_deal) || @dealer_hand.index(new_deal)
+    new_deal = CARD_VALUES.sample.to_s + " of " + SUITS.sample
   end
 
   if plyr == 'player'
     @player_hand << new_deal
   elsif plyr == 'dealer'
     @dealer_hand << new_deal
-  end 
+  end
 end
 
 def card_count(hnd)
   # iterate through each value of the array for count check
   # add card value into array for later sum
   count_arr = []
-  total = 0
   hnd.each do |x|
     if x.start_with?('K', 'Q', 'J', '10')
       count_arr << 10
@@ -38,23 +38,19 @@ def card_count(hnd)
     else
       count_arr << x[0].to_i
     end
-  end 
-  total = count_arr.inject(:+)
+  end
+  return count_arr.inject(:+)
 end
 
 def player_busted?
-  if (card_count(@player_hand)).to_i > 21
+  if (card_count(@player_hand)).to_i > MAGIC_NUMBER
     return true
-  else
-    nil  
-  end   
+  end
 end
 
 def dealer_busted?
-  if (card_count(@dealer_hand)).to_i > 21
+  if (card_count(@dealer_hand)).to_i > MAGIC_NUMBER
     return true
-  else
-    nil  
   end
 end
 
@@ -68,7 +64,7 @@ def compare_hands
   end
 end
 
-loop do  # 'play again' loop
+loop do # 'play again' loop
   @player_hand = []
   @dealer_hand = []
 
@@ -77,17 +73,17 @@ loop do  # 'play again' loop
   deal_card('player')
 
   deal_card('dealer')
+  deal_card('dealer')
 
   # initial hand display
-  prompt "Player Hand: #{@player_hand.join(", ")}"
+  prompt "Player Hand: #{@player_hand.join(', ')}"
 
-  prompt "Dealer Hand: #{@dealer_hand[0] + ", Face-Down Card"}"
+  prompt "Dealer Hand: #{@dealer_hand[0] + ', Face-Down Card'}"
 
-  loop do     # main game loop
-    answer = nil
+  loop do # main game loop
     prompt "Player's Turn"
     loop do
-      prompt "Player Hand: #{@player_hand.join(", ")}"
+      prompt "Player Hand: #{@player_hand.join(', ')}"
       prompt "Card count: #{card_count(@player_hand)}"
       break if player_busted?
       puts "Hit or stay?"
@@ -95,36 +91,37 @@ loop do  # 'play again' loop
       if answer.downcase.start_with?('s') || player_busted?
         break
       else
-        deal_card('player') 
+        deal_card('player')
       end
-    end  
+    end
 
     if player_busted?
       puts "Player BUSTS! Dealer WINS!"
       break
     else
       puts "Player stays!"
-    end 
+    end
 
-    answer = nil
     prompt "Dealer Turn"
     loop do
       break if dealer_busted?
       if card_count(@dealer_hand) < 17
+        prompt "Dealer hits!"
         deal_card('dealer')
       elsif card_count(@dealer_hand) > 17
         break
       end
-    end  
+    end
 
     if dealer_busted?
-      prompt "Dealer's Hand: #{@dealer_hand.join(", ")}"
-      prompt "Player's Hand: #{@player_hand.join(", ")}"
+      prompt "Dealer's Hand: #{@dealer_hand.join(', ')}"
+      prompt "Player's Hand: #{@player_hand.join(', ')}"
       puts "Dealer BUSTS! Player WINS!"
       break
     else
-      prompt "Dealer's Hand: #{@dealer_hand.join(", ")}"
-      prompt "Player's Hand: #{@player_hand.join(", ")}"
+      prompt "Dealer stays!"
+      prompt "Dealer's Hand: #{@dealer_hand.join(', ')}"
+      prompt "Player's Hand: #{@player_hand.join(', ')}"
       compare_hands
       break
     end
@@ -135,21 +132,3 @@ loop do  # 'play again' loop
 end
 
 prompt "Thanks for playing Blackjack!"
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
