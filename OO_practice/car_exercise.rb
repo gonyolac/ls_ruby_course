dea14192bd72f997a72fd99ed170bf7d36ed50a2
Carlo Gonzales <gonzales.caloy@gmail.com>
2015-12-29 15:29:36 -0500

    added practice exercises

--- OO_practice/car_exercise.rb
+++ OO_practice/car_exercise.rb
@@ -1,12 +1,30 @@
-class MyCar
+module Raceable
+  def race
+    "Race the Car!"
+  end
+end
+
+
+
+class Vehicle
+  @@number_of_vehicles = 0
   attr_accessor :color
-  attr_reader :year
+  attr_reader :model, :year
 
   def initialize (year, model, color) 
     @year = year
     @model = model
     @color = color
     @current_speed = 0
+    @@number_of_vehicles += 1
+  end
+
+  def self.number_of_vehicles
+    puts "This program has created #{@@number_of_vehicles} vehicles."
+  end
+
+  def self.calculate_gas(gallons, miles)
+    puts "#{miles / gallons} miles per gallon of gas"
   end
 
   def speed_up(number)
@@ -29,18 +47,67 @@ class MyCar
     puts "You new #{color} paint job looks great!"
   end
 
-  def self.calculate_gas(gallons, miles)
-    puts "#{miles / gallons} miles per gallon of gas"
+  def age
+    "The #{self.model} is #{years_old} years old." 
   end
 
-  def to_s
-    "My car is a #{color}, #{year}, #{model}"
+  private
+
+  def years_old
+    Time.now.year - self.year
   end
 
 end
+
+class MyTruck < Vehicle
+  NUMBER_OF_DOORS = 2
+
+  def to_s
+    "The truck is a #{self.color}, #{self.year}, #{self.model}."
+  end
+
+end
+
+class MyCar < Vehicle
+  NUMBER_OF_DOORS = 4 
+
+  include Raceable
+
+  def to_s
+    "The car is a #{self.color}, #{self.year}, #{self.model}."
+  end
+end
+
+class Student
+  def initialize(name, grade)
+    @name = name
+    @grade = grade
+  end
+
+  def better_grade_than?(other_student)
+    grade > other_student.grade
+  end
+
+  protected
+  
+  def grade
+    @grade
+  end
+
+end
+
+
+  
 
 accord = MyCar.new(2012, 'honda accord', 'white')
 accord.spray_paint('red')
 MyCar.calculate_gas(13, 351)
 puts my_car
+
+puts "---Vehicle Hierarchy---"
+puts Vehicle.ancestors
+puts "---MyTruck Hierarchy---"
+puts MyTruck.ancestors
+puts "---MyCar Hierarchy---"
+puts MyCar.ancestors
 
---
+++ OO_practice/student_exercise.rb
@@ -0,0 +1,17 @@
+class Student
+  def initialize(name, grade)
+    @name = name
+    @grade = grade
+  end
+
+  def better_grade_than?(other_student)
+    grade > other_student.grade
+  end
+
+  protected
+  
+  def grade
+    @grade
+  end
+
+end
