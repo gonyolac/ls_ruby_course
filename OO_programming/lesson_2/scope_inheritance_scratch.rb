badf315573ddd2e1df71591bf19a3ccf1993dcb2
Carlo Gonzales <gonzales.caloy@gmail.com>
2016-01-04 02:02:22 -0500

    renamed scratch

--- OO_programming/lesson_2/scope_inheritance_scratch.rb
+++ OO_programming/lesson_2/scope_inheritance_scratch.rb
@@ -4,8 +4,21 @@ class Animal
   end
 end
 
+module Swim
+  def enable_swimming
+    @can_swim = true
+  end
+end
+
+
 class Dog < Animal
+  include Swim
+
   def initialize(name); end  # <--- goes to this initialize (remember ancestors)
+
+  def swim
+    "swimming!" if @can_swim
+  end
 
   def dog_name
     "bark! bark! #{@name} bark! bark!"
@@ -13,5 +26,8 @@ class Dog < Animal
 end
 
 teddy = Dog.new("Teddy")
-puts teddy.dog_name
+teddy.enable_swimming
+puts teddy.swim
+
+# for module instance variables, call the method in the module first before accessing 
 
