49345e9c6b6c33135f33a29f52955dde99a9e4f7
Carlo Gonzales <gonzales.caloy@gmail.com>
2016-03-23 15:58:02 -0400

    refactored word_count live challenge

--- ruby_more_topics/weekly_challenges/word_count.rb
+++ ruby_more_topics/weekly_challenges/word_count.rb
@@ -1,11 +1,21 @@
 #use hash
 require 'pry'
 
-test = "The quick brown fox jumps over the lazy dog. Quick Brown Fox Dog dog Dog"
+test = 'Then Almitra spoke again and said..."And what of Marriage, master?" And he answered saying:
+You were born together, and together you shall be forevermore. You shall be together when the
+white wings of death scatter your days. Aye, you shall be together even in the silent memory of
+ God. But let there be spaces in your togetherness, And let the winds of the heavens dance
+ between you. Love one another, but make not a bond of love. Let it rather be a moving sea between
+the shores of your souls. Fill each other\'s cup but drink not from one cup. Give one another of
+ your bread but eat not from the same loaf. Sing and dance together and be joyous,
+but let each of you be alone, Even as the strings of a lute are alone though they quiver with the
+ same music. Give your hearts, but not into each other\'s keeping. For only the hand of Life can
+ contain your hearts. And stand together, yet not too near together. For the pillars of the
+ temple stand apart, And the oak tree and the cypress grow not in each other\'s shadow.'
  
 def word_count(text)
   final_list = Hash.new
-  filtered = text.gsub(/[^a-zA-Z\s]/, '')    #filter out all punctuations
+  filtered = text.gsub(/[^a-zA-Z\s]/, ' ')    #filter out all punctuations
   all_words = filtered.split
   # hash key must be the first occurence of a word in the text "Then..then" -> key: Then
   possible_keys = all_words.each do |x| # convert each word in array into hash key
