5078162cb7f65ebd41ade7479bdcbdc8d0b80120
Carlo Gonzales <gonzales.caloy@gmail.com>
2016-04-05 22:38:55 -0400

    refactored to_currency exercise

--- ruby_more_topics/weekly_challenges/to_currency.rb
+++ ruby_more_topics/weekly_challenges/to_currency.rb
@@ -1,15 +1,14 @@
 require 'pry'
 def to_currency(number_string)
-  rounded = number_string.to_f.round(2)
-  temp = rounded.to_s.split('.')
-  @change = temp[1]
-  @whole_dollar_amt = temp[0]
-  
+  raise ArgumentError if number_string =~ /[^\dD.]/
+  rounded = number_string.to_f.round(2).to_s.split('.')
+  @whole_dollar_amt = rounded[0]
+
+  @change = rounded[1]
+  @change.size < 2 ? @change << '0' : @change
+
   base = @whole_dollar_amt.split('').reverse.each_slice(3).to_a.reverse
-  by_threes = base.map {|x| x.inject(&:+)}
+  by_threes = base.map {|x| x.reverse.inject(&:+)}
   combined = by_threes.inject {|acc, chunks| acc + ',' + chunks}
   final = combined + '.' + @change
 end
-
-test_number = "99999.946"
-p to_currency(test_number)
