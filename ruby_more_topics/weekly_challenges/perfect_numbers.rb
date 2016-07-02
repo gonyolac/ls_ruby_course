922671afdf2f66a7489fa17626e33403e64beb72
Carlo Gonzales <gonzales.caloy@gmail.com>
2016-07-02 01:25:24 -0400

    updated perfect_number challenge

--- ruby_more_topics/weekly_challenges/perfect_numbers.rb
+++ ruby_more_topics/weekly_challenges/perfect_numbers.rb
@@ -1,8 +1,7 @@
 class PerfectNumber
   def self.classify(number)
     raise RuntimeError if number < 1
-    factors = []
-    (1.upto(number) {|x| (number % x == 0) ? factors.push(x) : nil})
+    factors = (1..number).select{|x| (number % x == 0)}
     (factors[0..-2]).inject(:+) == number ? 'perfect' : (number == 1 || factors[0..-2].inject(:+) < number ? 'deficient' : 'abundant')
   end
 end
