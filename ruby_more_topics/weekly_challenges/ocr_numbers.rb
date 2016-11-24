cb8f83f5677b2720c01f69332640e02e78419a0f
Carlo Gonzales <gonzales.caloy@gmail.com>
2016-11-24 14:46:48 -0500

    refactored ocr_numbers challenge

--- ruby_more_topics/weekly_challenges/ocr_numbers.rb
+++ ruby_more_topics/weekly_challenges/ocr_numbers.rb
@@ -11,22 +11,20 @@ class OCR
     7 => " _\n  |\n  |\n",
     8 => " _\n|_|\n|_|\n",
     9 => " _\n|_|\n _|\n"
-  }
+  }.freeze
+
   def initialize(text)
-    @text = text
+    @text = text.split(/(?<=\n)\n/)
   end
 
   def convert
-    @text.include?("\n\n") ? @text = @text.gsub("\n\n","\n\nx").split("\nx") : @text
-    @text.class == Array ? @text.map {|x| cipher(x)}.join(',') : cipher(@text)
+    @text.map {|x| lookup(x)}.join(',')
   end
 
   private
 
-  def cipher(binary)
-    base = binary.split("\n").map {|x| x.scan(/.{1,3}/)}
-    base.transpose.map {|x| REFERENCE.key(x.map{|y| y.rstrip << "\n" }.join) ? REFERENCE.key(x.map{|y| y.rstrip << "\n" }.join).to_s : "?" }.join
+  def lookup(binary_font)
+    base = binary_font.split("\n").map {|x| x.scan(/.{1,3}/)}.map {|x| x.empty? ? ["   "] : x}
+    base.transpose.map {|x| number = REFERENCE.key(x.map{|y| y.rstrip << "\n" }.join) ? REFERENCE.key(x.map{|y| y.rstrip << "\n" }.join).to_s : "?" }.join
   end
 end
-
-
